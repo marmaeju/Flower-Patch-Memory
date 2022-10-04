efaa16d4ecd82ac10bddd211020bcc4da0c0f866
Marley Judd <marleymaejudd@gmail.com>
2022-10-04 10:32:11 -0600

    trying to get the shuffle function to work

--- app.js
+++ app.js
@@ -1,14 +1,41 @@
-const cards = document.querySelectorAll('.card')
-const cardFaces = document.querySelectorAll('img')
+let cards = document.querySelectorAll('.card')
+let cardFace = document.querySelectorAll('.cardFace')
 const restartBtn = document.getElementsByClassName('restartBtn')
+
+console.log(cardFace)
+
+// let cardsArr = [...cards]
 
 // const gameStart = () => {}
 
 // const restartGame = () => {}
 
 // const match = () => {}
-
-// const shuffleCards = () => {}
+// const shuffle = (array) => {
+//   console.log(array)
+//   let curIndex = array.length - 1
+//   console.log(array[curIndex])
+//   while (curIndex !== 0) {
+//     let ranIndex = Math.floor(Math.random() * curIndex)
+//     console.log(ranIndex)
+//     let tempVal = array[curIndex]
+//     console.log(array[ranIndex])
+//     let newValue = array[ranIndex]
+//     console.log(newValue)
+//     array[curIndex] = newValue
+//     console.log(array[curIndex])
+//     array[ranIndex] = tempVal
+//     console.log(array[ranIndex])
+//     curIndex -= 1
+//     console.log(curIndex)
+//   }
+//   return array
+// }
+// cards = shuffle(cards)
+// console.log(cards)
+for (i = cardFace.length; i >= 0; i--) {
+  cards.appendChild(cardFace[(Math.random() * i) | 0])
+}
 
 cards.forEach((card) => {
   card.addEventListener('click', () => {
