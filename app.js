7c6805f27fcb3563dc448ac1b39eda9b953cae46
Marley Judd <marleymaejudd@gmail.com>
2022-10-04 14:19:53 -0600

    shuffle function working and updated psuedo code of how I did it

--- app.js
+++ app.js
@@ -1,14 +1,48 @@
 let cards = document.querySelectorAll('.card')
-let cardFace = document.querySelectorAll('.cardFace')
-let cornFlower = document.querySelectorAll('.cornFlower')
-let poppy = document.querySelectorAll('.poppy')
-let cosmos = document.querySelectorAll('.cosmos')
+let cardFaces = document.querySelectorAll('.cardFace')
+// let cornFlower = document.querySelectorAll('.cornFlower')
+// let poppy = document.querySelectorAll('.poppy')
+// let cosmos = document.querySelectorAll('.cosmos')
 const restartBtn = document.getElementsByClassName('restartBtn')
+/////////////////////////////
 
-console.log(cardFace)
-console.log(cornFlower)
-console.log(poppy)
-console.log(cosmos)
+// create two new empty arrays we can work with,
+// one is a copy of the node list we called earlier
+let copyCardFaces = []
+let shuffledCardFaces = []
+// iterate through node list and
+//push the elements from the node list to the copy array.
+for (i = 0; i < cardFaces.length; i++) {
+  copyCardFaces.push(cardFaces[i])
+  console.log(copyCardFaces)
+}
+// using the length of the original node list
+// iterate and through and splice out an image element randomly
+// using random index aka Math.floor(Math.random() * currentIndex)
+// push that into the shufflecCards array (the other new array we created)
+for (i = 0; i < cardFaces.length; i++) {
+  let curIndex = copyCardFaces.length
+  let ranIndex = Math.floor(Math.random() * curIndex)
+  console.log(ranIndex)
+  let splicedItem = copyCardFaces.splice(ranIndex, 1)
+  shuffledCardFaces.push(splicedItem[0])
+}
+console.log(shuffledCardFaces)
+
+for (i = 0; i < cards.length; i++) {
+  cards[i].innerHTML = ''
+  cards[i].appendChild(shuffledCardFaces[i])
+}
+
+// loop through the cards in the DOM to
+// remove the original innerHTML (imgs)
+// appendChild the shuffledCards array
+
+////////////////////////////
+// console.log(cardFaces)
+// console.log(cornFlower)
+// console.log(poppy)
+// console.log(cosmos)
 
 // let cardsArr = [...cards]
 
@@ -18,40 +52,14 @@ console.log(cosmos)
 
 // const match = () => {
 //   if (cornFlower === cornFlower) {
-//     card.children.item(0).style.display = 'block'
+//     cornFlower.style.display = 'block'
 //   } else if (poppy === poppy) {
-//     card.children.item(0).style.display = 'block'
+//     poppy.style.display = 'block'
 //   } else if (comsos === cosmos) {
-//     card.children.item(0).style.display = 'block'
+//     cosmos.style.display = 'block'
 //   } else {
-//     card.children.item(0).style.display = 'none'
+//     cardFaces.style.display = 'none'
 //   }
-// }
-
-// const shuffle = (array) => {
-//   console.log(array)
-//   let curIndex = array.length - 1
-//   console.log(array[curIndex])
-//   while (curIndex !== 0) {
-//     let ranIndex = Math.floor(Math.random() * curIndex)
-//     console.log(ranIndex)
-//     let tempVal = array[curIndex]
-//     console.log(array[ranIndex])
-//     let newValue = array[ranIndex]
-//     console.log(newValue)
-//     array[curIndex] = newValue
-//     console.log(array[curIndex])
-//     array[ranIndex] = tempVal
-//     console.log(array[ranIndex])
-//     curIndex -= 1
-//     console.log(curIndex)
-//   }
-//   return array
-// }
-// cards = shuffle(cards)
-// console.log(cards)
-// for (i = cardFace.length; i >= 0; i--) {
-//   cards.appendChild(cardFace[(Math.random() * i) | 0])
 // }
 
 cards.forEach((card) => {
