9a64d91e46d70d9ea6130ab88aff4bec47326d37
Marley Judd <marleymaejudd@gmail.com>
2022-10-05 10:02:40 -0600

    looped through and added an event listener to each card adding a class so that it will help in the matching process

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ for (i = 0; i < cardFaces.length; i++) {
 for (i = 0; i < cards.length; i++) {
   cards[i].innerHTML = ''
   cards[i].appendChild(shuffledCardFaces[i])
+  cards[i].classList.add(shuffledCardFaces[i].classList[1])
 }
 // }
 
@@ -43,7 +44,15 @@ console.log(cosmos)
 let cornFlowerArr = []
 let poppyArr = []
 let cosmosArr = []
+let clickedCard = ''
 
+for (i = 0; i < cards.length; i++) {
+  cards[i].addEventListener('click', () => {
+    clickedCard = cards[i].classList[1]
+    console.log(cards[i])
+    console.log(clickedCard)
+  })
+}
 for (i = 0; i < cornFlower.length; i++) {
   cornFlowerArr.push(cornFlower[i])
 }
@@ -56,13 +65,20 @@ for (i = 0; i < cosmos.length; i++) {
   cosmosArr.push(cosmos[i])
 }
 
-if (cornFlowerArr[0] === cornFlowerArr[1]) {
-} else if (poppyArr[0] === poppyArr[1]) {
-} else if (cosmosArr[0] === cosmosArr[1]) {
-} else {
+if (cards.length === 2 && cornFlowerArr[0] === cornFlowerArr[1]) {
+  console.log('you made a match')
+} else if (cards.length === 2 && poppyArr[0] === poppyArr[1]) {
+  console.log('you made a match')
+} else if (cards.length === 2 && cosmosArr[0] === cosmosArr[1]) {
+  console.log('you made a match')
+  // } else if (cardFaces.style.display === 'block') {
+  //   console.log('you did it')
+  // } else {
+  //   cardFaces.style.display = 'none'
+  //   console.log('try again')
 }
 
-console.log(shuffledCardFaces)
+console.log(cards)
 // }
 
 // const gameStart = () => {
