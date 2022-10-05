12880f6f0819421cdcac0f85fb51615cc390855b
Marley Judd <marleymaejudd@gmail.com>
2022-10-05 13:59:07 -0600

    not a match condition created

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 let cards = document.querySelectorAll('.card')
 let cardFaces = document.querySelectorAll('.cardFace')
-let cornFlower = document.querySelectorAll('.cornFlower')
-let poppy = document.querySelectorAll('.poppy')
-let cosmos = document.querySelectorAll('.cosmos')
 let announcer = document.querySelector('.announce')
 const restartBtn = document.querySelector('.restartBtn')
 
@@ -30,10 +27,7 @@ for (i = 0; i < cards.length; i++) {
   cards[i].appendChild(shuffledCardFaces[i])
   cards[i].classList.add(shuffledCardFaces[i].classList[1])
   cards[i].addEventListener('click', (event) => {
-    // clickedCard = cards[i].classList[1]
-    // console.log(clickedCard)
     event.currentTarget.children.item(0).style.display = 'block'
-    console.log(event.currentTarget.classList.contains('cosmos'))
     if (tally < 2) {
       tally++
       if (event.currentTarget.classList.contains('cosmos')) {
@@ -43,19 +37,25 @@ for (i = 0; i < cards.length; i++) {
         cornFlowerCounter++
         console.log('this is corn flower', cornFlowerCounter)
       } else if (event.currentTarget.classList.contains('poppy')) {
+        poppyCounter++
+        console.log('this is poppy', poppyCounter)
       }
       if (tally === 2) {
         if (cosmosCounter === 2) {
           console.log('youve matched cosmos')
         } else if (cornFlowerCounter === 2) {
-          console.log("you've matched corn flower")
+          console.log('youve matched corn flower')
         } else if (poppyCounter === 2) {
           console.log('youve matched poppy')
+        } else {
+          console.log('not a match')
         }
+
         // event.currentTarget.children.item(0).style.display = 'none'
         tally = 0
         cosmosCounter = 0
         cornFlowerCounter = 0
+        poppyCounter = 0
         console.log(tally)
       }
     }
@@ -75,46 +75,13 @@ for (i = 0; i < cards.length; i++) {
 
 // }
 
-// const match = () => {
-let cornFlowerArr = []
-let poppyArr = []
-let cosmosArr = []
-let clickedCard = ''
-
-// for (i = 0; i < cards.length; i++) {}
-// for (i = 0; i < cornFlower.length; i++) {
-//   cornFlowerArr.push(cornFlower[i])
-// }
-
-// for (i = 0; i < poppy.length; i++) {
-//   poppyArr.push(poppy[i])
-// }
-
-// for (i = 0; i < cosmos.length; i++) {
-//   cosmosArr.push(cosmos[i])
-// }
-
-// if (cards.length === 2 && cornFlowerArr[0] === cornFlowerArr[1]) {
-//   announcer.innerHTML = 'You made a match!'
-// } else if (cards.length === 2 && poppyArr[0] === poppyArr[1]) {
-//   announcer.innerHTML = 'You made a match!'
-// } else if (cards.length === 2 && cosmosArr[0] === cosmosArr[1]) {
-//   announcer.innerHTML = 'You made a match!'
-// } else if (cardFaces.style.display === 'block') {
-//   console.log('you did it')
-// } else {
-//   cardFaces.style.display = 'none'
-//   console.log('try again')
-// }
-
-console.log(cards)
-// }
-
 // const gameStart = () => {
 //   shuffle()
 //
 // }
 
-// const restartGame = () => {}
+// restartBtn.addEventListener('click', () => {
+//   cards.innerHTML = ''
+// })
 
 // gameStart()
