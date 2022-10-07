0a67dc56d1976f84b0a0579a29981fae6cb35a03
Marley Judd <marleymaejudd@gmail.com>
2022-10-06 20:18:40 -0600

    added finished button to show winning message

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let cards = document.querySelectorAll('.card')
 let cardFaces = document.querySelectorAll('.cardFace')
 let announcer = document.querySelector('.announce')
-const restartBtn = document.querySelector('.restartBtn')
+const finishBtn = document.querySelector('.finishBtn')
 
 let copyCardFaces = []
 let shuffledCardFaces = []
@@ -19,78 +19,78 @@ let flip2 = (cardDiv) => {
   cardDiv.children.item(0).style.display = 'none'
 }
 let previousTarget = ''
-// let shuffle = () => {
-for (i = 0; i < cardFaces.length; i++) {
-  copyCardFaces.push(cardFaces[i])
+
+let shuffle = () => {
+  for (i = 0; i < cardFaces.length; i++) {
+    copyCardFaces.push(cardFaces[i])
+  }
+  for (i = 0; i < cardFaces.length; i++) {
+    let curIndex = copyCardFaces.length
+    let ranIndex = Math.floor(Math.random() * curIndex)
+    let splicedItem = copyCardFaces.splice(ranIndex, 1)
+    shuffledCardFaces.push(splicedItem[0])
+  }
 }
-for (i = 0; i < cardFaces.length; i++) {
-  let curIndex = copyCardFaces.length
-  let ranIndex = Math.floor(Math.random() * curIndex)
-  let splicedItem = copyCardFaces.splice(ranIndex, 1)
-  shuffledCardFaces.push(splicedItem[0])
-}
-for (i = 0; i < cards.length; i++) {
-  cards[i].innerHTML = ''
-  cards[i].appendChild(shuffledCardFaces[i])
-  cards[i].classList.add(shuffledCardFaces[i].classList[1])
-  cards[i].addEventListener('click', (event) => {
-    event.currentTarget.children.item(0).style.display = 'block'
-    if (tally < 1) {
-      tally++
-      if (event.currentTarget.classList.contains('cosmos')) {
-        cosmosCounter++
-        previousTarget = event.currentTarget.id
-      } else if (event.currentTarget.classList.contains('cornFlower')) {
-        cornFlowerCounter++
-        previousTarget = event.currentTarget.id
-      } else if (event.currentTarget.classList.contains('poppy')) {
-        poppyCounter++
-        previousTarget = event.currentTarget.id
-      }
-    } else if (tally < 2) {
-      tally++
-      if (event.currentTarget.classList.contains('cosmos')) {
-        cosmosCounter++
-      } else if (event.currentTarget.classList.contains('cornFlower')) {
-        cornFlowerCounter++
-      } else if (event.currentTarget.classList.contains('poppy')) {
-        poppyCounter++
-      }
-      if (tally === 2) {
-        if (cosmosCounter === 2) {
-          announcer.innerHTML = "You've picked Cosmos!"
-        } else if (cornFlowerCounter === 2) {
-          announcer.innerHTML = "You've gathered Cornflowers!"
-        } else if (poppyCounter === 2) {
-          announcer.innerHTML = "You've collected Poppies!"
-        } else {
-          let otherCard = document.getElementById(previousTarget)
-          console.log(otherCard)
-          setTimeout(() => flip2(otherCard), 1000)
-          setTimeout(() => cardFlip(event), 1000)
-          announcer.innerHTML = 'Try again!'
+let winGame = () => {
+  for (i = 0; i < cards.length; i++) {
+    cards[i].innerHTML = ''
+    cards[i].appendChild(shuffledCardFaces[i])
+    cards[i].classList.add(shuffledCardFaces[i].classList[1])
+    cards[i].addEventListener('click', (event) => {
+      event.currentTarget.children.item(0).style.display = 'block'
+      if (tally < 1) {
+        tally++
+        if (event.currentTarget.classList.contains('cosmos')) {
+          cosmosCounter++
+          previousTarget = event.currentTarget.id
+        } else if (event.currentTarget.classList.contains('cornFlower')) {
+          cornFlowerCounter++
+          previousTarget = event.currentTarget.id
+        } else if (event.currentTarget.classList.contains('poppy')) {
+          poppyCounter++
+          previousTarget = event.currentTarget.id
         }
-        tally = 0
-        cosmosCounter = 0
-        cornFlowerCounter = 0
-        poppyCounter = 0
-        console.log(tally)
+      } else if (tally < 2) {
+        tally++
+        if (event.currentTarget.classList.contains('cosmos')) {
+          cosmosCounter++
+        } else if (event.currentTarget.classList.contains('cornFlower')) {
+          cornFlowerCounter++
+        } else if (event.currentTarget.classList.contains('poppy')) {
+          poppyCounter++
+        }
+        if (tally === 2) {
+          if (cosmosCounter === 2) {
+            announcer.innerHTML = "You've picked Cosmos!"
+          } else if (cornFlowerCounter === 2) {
+            announcer.innerHTML = "You've gathered Cornflowers!"
+          } else if (poppyCounter === 2) {
+            announcer.innerHTML = "You've collected Poppies!"
+          } else {
+            let otherCard = document.getElementById(previousTarget)
+            console.log(otherCard)
+            setTimeout(() => flip2(otherCard), 800)
+            setTimeout(() => cardFlip(event), 800)
+            announcer.innerHTML = 'Try again!'
+          }
+          tally = 0
+          cosmosCounter = 0
+          cornFlowerCounter = 0
+          poppyCounter = 0
+          console.log(tally)
+        }
       }
-    }
-  })
+    })
+  }
 }
 
-// // })
+const gameStart = () => {
+  shuffle()
+  winGame()
+}
 
-// }
-
-// const gameStart = () => {
-//   shuffle()
-//
-// }
-
-restartBtn.addEventListener('click', () => {
-  console.log('clicked')
+finishBtn.addEventListener('click', () => {
+  announcer.innerHTML = 'You gathered all the flowers! Congratulations!'
 })
 
-// gameStart()
+gameStart()
